697b1655d8c702ea20405fb235074474951774d7
eeshaan <me@eeshaan.life>
2020-04-18 09:56:40 -0500

    update Main.java to add basic FX scene

--- application/Main.java
+++ application/Main.java
@@ -4,6 +4,9 @@
 package application;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 /**
@@ -12,18 +15,29 @@ import javafx.stage.Stage;
  */
 public class Main extends Application {
 
+  private static final String APP_TITLE = "Crystal";
+  private static final int WINDOW_WIDTH = 1144;
+  private static final int WINDOW_HEIGHT = 880;
+  
   @Override
-  public void start(Stage arg0) throws Exception {
-    // TODO Auto-generated method stub
+  public void start(Stage mainStage) throws Exception {
+    // Main layout is Border Pane example (top,left,center,right,bottom)
+    BorderPane root = new BorderPane();
+    root.setPadding(new Insets(10, 10 , 10, 10));
+    
+    Scene mainScene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
 
+    // Add the stuff and set the primary stage
+    mainStage.setTitle(APP_TITLE);
+    mainStage.setScene(mainScene);
+    mainStage.show();
   }
 
   /**
    * @param args
    */
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
-
+    launch(args);
   }
 
 }
