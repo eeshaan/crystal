8a38b9b61ccf743c557bd10e77f7c598148d04ab
eeshaan <me@eeshaan.life>
2020-04-20 16:20:34 -0500

    🕉refactor @bendantan's code to fix header and allow for dynamic layout

--- application/Main.java
+++ application/Main.java
@@ -42,192 +42,197 @@ public class Main extends Application {
   private static final String APP_TITLE = "Crystal";
   private static final int WINDOW_WIDTH = 1144; // divide Figma by 2.039335664
   private static final int WINDOW_HEIGHT = 880;
-  
+
   @Override
   public void start(Stage mainStage) throws Exception {
     // Main layout is Border Pane example (top,left,center,right,bottom)
     BorderPane root = new BorderPane();
     root.setPadding(new Insets(0, 0, 0, 0));
-    
+
     // Due Today Header
     Text dueTodayHeader = new Text("Due Today");
     dueTodayHeader.setId("h1"); // apply DM Serif Display font
- 
+
     // Continue working on Header
     Text workHeader = new Text("Continue working on");
     workHeader.setId("h1");
-    
+
     // Create Left Pane
-    VBox leftPane = new VBox();
+    FlowPane leftPane = new FlowPane();
     leftPane.setPrefWidth(750);
-    leftPane.getStyleClass().add("leftPane");
-    leftPane.setPadding(new Insets(0, 65, 0, 65));
-    leftPane.getChildren().add(dueTodayHeader);
-    
-//Add today's tasks. Will need to be put into a for loop eventually
-    //task 1
+
+    // Create Assignments Pane
+    VBox assignmentsPane = new VBox();
+    assignmentsPane.setPrefSize(750, 668);
+    assignmentsPane.getStyleClass().add("assignmentsPane");
+    assignmentsPane.setPadding(new Insets(0, 65, 0, 65));
+    assignmentsPane.getChildren().add(dueTodayHeader);
+
+    ///// Add today's tasks. Will need to be put into a for loop eventually /////
+    // task 1
     HBox assignmentBox1 = new HBox();
     assignmentBox1.getStyleClass().add("assignmentBox");
     assignmentBox1.setId("ass_MATH222");
-    
+
     Text time1 = new Text("11:00 AM");
     time1.setId("time");
-    
+
     Text desc1 = new Text(" — 11.6 little-o notation");
-    
+
     assignmentBox1.getChildren().addAll(time1, desc1);
-    
-    leftPane.getChildren().add(assignmentBox1);
-    
-    //task 2
+
+    assignmentsPane.getChildren().add(assignmentBox1);
+
+    // task 2
     HBox assignmentBox2 = new HBox();
     assignmentBox2.getStyleClass().add("assignmentBox");
     assignmentBox2.setId("ass_CS400");
-    
+
     Text time2 = new Text("11:59 PM");
     time2.setId("time");
-    
+
     Text desc2 = new Text(" — p6");
     assignmentBox2.getChildren().addAll(time2, desc2);
-    leftPane.setSpacing(5.0);
-    
-    leftPane.getChildren().add(assignmentBox2);
-    
-    leftPane.getChildren().add(workHeader);
-    
-//Add the rest of the tasks/the next few tasks. Also must be a for loop
-    //task 3
+    assignmentsPane.setSpacing(5.0);
+
+    assignmentsPane.getChildren().add(assignmentBox2);
+
+    ///// Add the rest of the tasks/the next few tasks. Also must be a for loop /////
+
+    assignmentsPane.getChildren().add(workHeader);
+
+    // task 3
     HBox assignmentBox3 = new HBox();
     assignmentBox3.getStyleClass().add("assignmentBox");
     assignmentBox3.setId("ass_CS252");
-    
+
     Text time3 = new Text("Friday, April 17 at 11:59 PM");
     time3.setId("time");
-    
+
     Text desc3 = new Text(" — Worksheet 12");
     assignmentBox3.getChildren().addAll(new Text("Due "), time3, desc3);
 
-    leftPane.getChildren().add(assignmentBox3);
-    
-    //task 4
+    assignmentsPane.getChildren().add(assignmentBox3);
+
+    // task 4
     HBox assignmentBox4 = new HBox();
     assignmentBox4.getStyleClass().add("assignmentBox");
     assignmentBox4.setId("ass_PHILOS101");
-    
+
     Text time4 = new Text("Sunday, April 19 at 8:00 PM");
     time4.setId("time");
-    
+
     Text desc4 = new Text(" — Meaning of Life Essay");
     assignmentBox4.getChildren().addAll(new Text("Due "), time4, desc4);
 
-    leftPane.getChildren().add(assignmentBox4);
-    
- 
-    
-//Setting up regions for spacing
-    //Horizontal region for spacing
+    assignmentsPane.getChildren().add(assignmentBox4);
+
+
+
+    // Setting up regions for spacing
+    // Horizontal region for spacing
     Region hRegion = new Region();
+    hRegion.setPrefWidth(120);
     HBox.setHgrow(hRegion, Priority.ALWAYS);
-    
-    //Vertical region for spacing
-    Region vRegion = new Region();
-    VBox.setVgrow(vRegion, Priority.ALWAYS);
-    
-//Setting Children for leftPaneHeader
-    
-    //date display
+
+
+    ///// Setting Children for leftPaneHeader /////
+
+    // date display
     SimpleDateFormat dateFormat = new SimpleDateFormat("EEEE, MMMMM d, yyyy");
     Date now = new Date(System.currentTimeMillis());
     Text dateText = new Text(dateFormat.format(now));
     dateText.setId("dateText");
-    
-    //"What should I do now" button
+
+    // "What should I do now" button
     Button task = new Button("What should I do now?");
-    task.setStyle("-fx-background-color: red; -fx-height: 26;"); //styling
-    
-    //search button
+    task.setId("task");
+    task.setStyle("-fx-background-color: red; -fx-height: 26;"); // styling
+
+    // search button
     Image searchImage = new Image("/application/src/img/search-icon.png", 26, 26, true, true);
     ImageView search = new ImageView();
     search.setImage(searchImage);
-    Button searchBtn = new Button("",search);
-    searchBtn.setStyle("-fx-background-color: transparent;");//styling
-    
-    
-    //window button
+    Button searchBtn = new Button("", search);
+    searchBtn.getStyleClass().add("iconBtn");
+
+
+    // window button
     Image layoutImage = new Image("/application/src/img/layout-icon.png", 26, 26, true, true);
     ImageView layout = new ImageView();
     layout.setImage(layoutImage);
-    Button windowBtn = new Button("",layout);
-    windowBtn.setStyle("-fx-background-color: transparent;");//styling
-    
-//Setting HBox for add button
+    Button windowBtn = new Button("", layout);
+    windowBtn.getStyleClass().add("iconBtn");
+
+    // Setting HBox for add button
     HBox addBox = new HBox();
     Image addImage = new Image("/application/src/img/add-icon.png", 56, 56, true, true);
     ImageView add = new ImageView();
     add.setImage(addImage);
-    Button addBtn = new Button("",add);
-    addBtn.setStyle("-fx-background-color: transparent;");//styling
+    Button addBtn = new Button("", add);
+    addBtn.getStyleClass().add("iconBtn");
     addBox.getChildren().add(addBtn);
-    addBtn.setAlignment(Pos.BOTTOM_RIGHT);
-    addBox.setMargin(addBtn, new Insets(20,20,20,20));
-    
-    
-//setting up header HBox
+
+    // setting up header HBox
     HBox leftPaneHeader = new HBox(dateText, hRegion, task, searchBtn, windowBtn);
     leftPaneHeader.setPrefHeight(106);
-    leftPaneHeader.setId("leftPaneHeader");
+    leftPaneHeader.setPadding(new Insets(40, 65, 40, 65));
     leftPaneHeader.setAlignment(Pos.CENTER);
-    leftPaneHeader.setMargin(dateText, new Insets(0,0,0,70));
+    leftPaneHeader.setId("leftPaneHeader");
 
-    leftPane.getChildren().addAll(leftPaneHeader, vRegion, addBox);
-    addBox.setAlignment(Pos.BOTTOM_RIGHT);
-    
+    HBox leftPaneFooter = new HBox(addBtn);
+    leftPaneFooter.setPrefHeight(106);
+    leftPaneFooter.setPadding(new Insets(40, 65, 40, 65));
+    leftPaneFooter.setAlignment(Pos.CENTER);
+
+    leftPane.getChildren().addAll(leftPaneHeader, assignmentsPane, leftPaneFooter);
+
     root.setLeft(leftPane);
-    
-    
-// Create rightPane
+
+
+    // Create rightPane
     VBox rightPane = new VBox();
     rightPane.setPrefWidth(394);
     rightPane.setId("rightPane");
-    
+
     HBox rightPaneHeader = new HBox();
     rightPaneHeader.setPrefHeight(106);
     rightPaneHeader.setId("rightPaneHeader");
     rightPane.getChildren().add(rightPaneHeader);
-    
+
     Pane calendarView = new Pane();
     calendarView.setPrefHeight(427.59);
     calendarView.setId("calendarView");
     rightPane.getChildren().add(calendarView);
-    
+
     DatePicker dp = new DatePicker(LocalDate.now());
     DatePickerSkin datePickerSkin = new DatePickerSkin(dp);
     Node popupContent = datePickerSkin.getPopupContent();
-    
+
     popupContent.setScaleX(1.3);
     popupContent.setLayoutX(50);
-    
+
     popupContent.setScaleY(1.3);
     popupContent.setLayoutY(-13);
-    
+
     calendarView.getChildren().add(popupContent);
-    
-    
+
+
     FlowPane subjectsPane = new FlowPane();
     subjectsPane.setPrefHeight(346.41);
     subjectsPane.setPadding(new Insets(60, 50, 60, 50));
     rightPane.getChildren().add(subjectsPane);
-    
+
     Text yourSubjects = new Text("Your subjects");
     yourSubjects.setId("yourSubjects");
-    
+
     HBox subjectsHeader = new HBox();
     subjectsHeader.getChildren().add(yourSubjects);
-    
+
     FlowPane subjects = new FlowPane();
     subjects.setPrefWidth(294);
     subjects.getStyleClass().add("subjects");
-    
+
     Button subject1 = new Button("MATH 222");
     subject1.setId("MATH222");
     Button subject2 = new Button("CS 400");
@@ -236,15 +241,16 @@ public class Main extends Application {
     subject3.setId("PHILOS101");
     Button subject4 = new Button("CS 252");
     subject4.setId("CS252");
-    
+
     subjects.getChildren().addAll(subject1, subject2, subject3, subject4);
-    
+
     subjectsPane.getChildren().addAll(subjectsHeader, subjects);
-    
+
     root.setRight(rightPane);
-    
+
     Scene mainScene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
-    mainScene.getStylesheets().add(getClass().getResource("/application/src/css/style.css").toExternalForm());
+    mainScene.getStylesheets()
+        .add(getClass().getResource("/application/src/css/style.css").toExternalForm());
 
     // Add the stuff and set the primary stage
     mainStage.setTitle(APP_TITLE);
