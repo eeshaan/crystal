2de80aea0e0979db9135500d636533c7844130e0
Ben <bendantan@gmail.com>
2020-04-21 18:51:16 -0500

    UI Elements added

--- application/SubjectsManagerWindow.java
+++ application/SubjectsManagerWindow.java
@@ -5,21 +5,33 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class SubjectsManagerWindow {
 
   private static final int WINDOW_WIDTH = 680;
-  private static final int WINDOW_HEIGHT = 400;
+  private static final int WINDOW_HEIGHT = 600;
   
   static int i = 1;
   
   public static void newWindow(String title) {
     Stage window = new Stage();
     window.initModality(Modality.APPLICATION_MODAL);
+    
+    BorderPane root = new BorderPane();
+    
+    HBox headerBox = new HBox();
+    Text header = new Text("Welcome to Crystal!");
+    headerBox.getChildren().add(header);
+    
+    root.setTop(headerBox);
+    
 
     GridPane pane = new GridPane();
 
@@ -36,14 +48,26 @@ public class SubjectsManagerWindow {
         difficulty[i] = new Spinner<>(1, 5, 3, 1);
         difficulty[i].getStyleClass().add(Spinner.STYLE_CLASS_SPLIT_ARROWS_HORIZONTAL);
         
-        pane.add(textField[i], 0, i);
-        pane.add(cp[i], 5, i);
-        pane.add(difficulty[i], 7, i);
+        pane.add(new Text("Class Name:"), 0, (i*2)-1);
+        pane.add(textField[i], 0, i*2);
+        pane.add(new Text("Color:"), 5, (i*2)-1);
+        pane.add(cp[i], 5, i*2);
+        pane.add(new Text("Difficulty (1-5):"), 7, (i*2)-1);
+        pane.add(difficulty[i], 7, i*2);
         i++;
 
     });
     
-    Scene scene = new Scene(pane, WINDOW_WIDTH, WINDOW_HEIGHT);
+    root.setCenter(pane);
+    
+    HBox bottom = new HBox();
+    Button submit = new Button("Submit classes and launch");
+    Button skip = new Button("Skip adding classes and launch");
+    bottom.getChildren().addAll(submit, skip);
+    
+    root.setBottom(bottom);
+    
+    Scene scene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
     scene.getStylesheets().add(AddAssignmentWindow.class.getResource("/application/src/css/style.css").toExternalForm()); // link CSS
     
     window.setScene(scene);
