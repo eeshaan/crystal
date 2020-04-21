6d05a4b955229abd87bbb3a2010374e58d126a29
eeshaan <me@eeshaan.life>
2020-04-21 12:12:48 -0500

    reorganize .setSpacing()
    this is probably the reason why padding isn't working, but will look into it
    
    Co-Authored-By: Benjamin Tan <bendantan@users.noreply.github.com>

--- application/Main.java
+++ application/Main.java
@@ -70,6 +70,7 @@ public class Main extends Application {
     assignmentsPane = new VBox();
     assignmentsPane.setPrefSize(750, 668);
     assignmentsPane.getStyleClass().add("assignmentsPane");
+    assignmentsPane.setSpacing(5.0);
     assignmentsPane.setPadding(new Insets(0, 65, 0, 65));
     assignmentsPane.getChildren().add(dueTodayHeader);
 
@@ -98,7 +99,6 @@ public class Main extends Application {
 
     Text desc2 = new Text(" - p6");
     assignmentBox2.getChildren().addAll(time2, desc2);
-    assignmentsPane.setSpacing(5.0);
 
     assignmentsPane.getChildren().add(assignmentBox2);
 
