52d8389fa2369788d5afbe340a7fecac67114f41
eeshaan <me@eeshaan.life>
2020-04-23 22:45:43 -0500

    add .json FileChooser

--- application/WelcomeWindow.java
+++ application/WelcomeWindow.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.io.File;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -8,6 +9,7 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
+import javafx.stage.FileChooser;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -20,31 +22,44 @@ public class WelcomeWindow {
     Text welcomeText = new Text(title);
     welcomeText.setTextAlignment(TextAlignment.CENTER);
     welcomeText.setId("h1");
-    
+
     HBox welcomeHeader = new HBox();
     welcomeHeader.setAlignment(Pos.CENTER);
     welcomeHeader.getChildren().add(welcomeText);
-    
+
     Button load = new Button("I've used Crystal before. (Load saved state from JSON.)");
-    
+
     HBox loadButtonHolder = new HBox();
     loadButtonHolder.setAlignment(Pos.CENTER);
     loadButtonHolder.getChildren().add(load);
-    
-    Button addClasses = new Button("This is my first time. (Start adding classes and assignments from scratch.)");
-    
+
+
+
+    FileChooser fileChooser = new FileChooser();
+
+    fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("JSON Files", "*.json"));
+
+    load.setOnAction(e -> {
+      File selectedFile = fileChooser.showOpenDialog(window);
+    });
+
+
+    Button addClasses =
+        new Button("This is my first time. (Start adding classes and assignments from scratch.)");
+
     HBox addButtonHolder = new HBox();
     addButtonHolder.setAlignment(Pos.CENTER);
     addButtonHolder.getChildren().add(addClasses);
-    
+
     VBox pane = new VBox();
     pane.setPadding(new Insets(20, 20, 20, 20));
-    
+
     pane.getChildren().addAll(welcomeHeader, loadButtonHolder, addButtonHolder);
-    
+
     Scene scene = new Scene(pane, 500, 500);
-    scene.getStylesheets().add(Main.class.getResource("/application/src/css/style.css").toExternalForm()); // link CSS
-    
+    scene.getStylesheets()
+        .add(Main.class.getResource("/application/src/css/style.css").toExternalForm()); // link CSS
+
     window.setScene(scene);
     window.setTitle(title);
     window.showAndWait();
