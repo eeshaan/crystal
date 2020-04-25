2958f7f288289b3f8b1ff4e7f8437d0e7536836d
unknown <acfranz2@gmail.com>
2020-04-25 10:54:24 -0400

    add basic class structure

---
+++ application/LinkedList.java
@@ -0,0 +1,35 @@
+package application;
+
+public class LinkedList implements LinkedListADT {
+
+	@Override
+	public void insert() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object remove() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object get() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean contains(Object data) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean size() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
