97160a220ec87a2914a395820b0a00739b1b8c21
eeshaan <me@eeshaan.life>
2020-04-30 20:52:03 -0500

    add comments to Queue
    Co-Authored-By: Mihaya Hotsumi <mihayahotsumiamln@users.noreply.github.com>

--- application/Queue.java
+++ application/Queue.java
@@ -2,24 +2,40 @@ package application;
 
 public class Queue implements QueueADT<Assignment> {
 
+  /**
+   * enqueue - adds an object to the queue
+   * @param data - object added to the queue
+   */
   @Override
   public void enqueue(Assignment data) {
     // TODO Auto-generated method stub
 
   }
 
+  /**
+   * dequeue - removes the first object in the queue
+   * @return the first Assignment in the queue
+   */
   @Override
   public Assignment dequeue() {
     // TODO Auto-generated method stub
     return null;
   }
 
+  /**
+   * size - gets the size of the queue
+   * @return int representing the size of the queue
+   */
   @Override
   public int size() {
     // TODO Auto-generated method stub
     return 0;
   }
 
+  /**
+   * contains - checks if an Assignment is in the queue
+   * @return true if the assignment is in the queue and false otherwise
+   */
   @Override
   public boolean contains(Assignment data) {
     // TODO Auto-generated method stub
--- application/QueueADT.java
+++ application/QueueADT.java
@@ -2,11 +2,28 @@ package application;
 
 public interface QueueADT<T> {
 
+  /**
+   * enqueue - adds an object to the queue
+   * @param data - the object added to the queue
+   */
   public void enqueue(T data);
 
+  /**
+   * dequeue - removes the first object in the queue
+   * @return the object removed from the queue
+   */
   public T dequeue();
 
+  /**
+   * size - gets the size of the queue
+   * @return the number of elements in the queue (int)
+   */
   public int size();
 
+  /**
+   * contains - checks if the object is in the queue
+   * @param data - the object checked if in the queue
+   * @return true if the object is in the queue and false otherwise
+   */
   public boolean contains(T data);
 }
