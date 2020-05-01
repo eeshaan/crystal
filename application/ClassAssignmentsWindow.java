28a60713510e198ece5cc3ce403e12d1ffa5f958
eeshaan <me@eeshaan.life>
2020-05-01 15:16:22 -0500

    fix assignments by class

--- application/ClassAssignmentsWindow.java
+++ application/ClassAssignmentsWindow.java
@@ -14,12 +14,14 @@ import javafx.stage.Stage;
 /**
  * 
  * CourseAssignmentsWindow - TODO Describe purpose of this user-defined type
+ * 
  * @author Ayaz Franz, Benjamin Tan, Bryan Lin, Devin Demirlika, Eeshaan Pirani
  */
 public class ClassAssignmentsWindow {
 
   /**
    * Creates the Course Assignments window
+   * 
    * @param className - name of the class
    */
   public static void newWindow(Class className) {
@@ -39,48 +41,47 @@ public class ClassAssignmentsWindow {
     assignmentsPane.setSpacing(5);
     assignmentsPane.setPadding(new Insets(20, 20, 20, 20));
 
-    // Gets all assignments belonging to the class
-    if (assignmentsByClass.get(className).size() == 0) {
+    Iterator<Assignment> today = assignmentsByClass.get(className).iterator();
+
+    // Creates an assignment box for each assignment in the class
+    while (today != null && today.hasNext()) {
+      Assignment assignment = today.next(); // Gets assignments with nearest due date
+      String name = assignment.getAssignmentName(); // Gets the assignment's name
+
+      // Creates an hbox and formats it
+      HBox assignmentBox = new HBox();
+      assignmentBox.getStyleClass().add("assignmentBox");
+
+      // Sets the color of the assignment to the class color
+      int[] classColor = assignment.getClassName().getClassColor();
+      assignmentBox.setStyle("-fx-background-color: rgba(" + classColor[0] + ", " + classColor[1]
+          + ", " + classColor[2] + ", 0.15); -fx-border-color: rgb(" + classColor[0] + ", "
+          + classColor[1] + ", " + classColor[2] + ");");
+
+      // When clicked, the assignment information appears
+      assignmentBox.setOnMouseClicked(e -> {
+        Main.assignmentOptions(assignmentBox, assignment);
+      });
+
+      // Creates text for assignment due date
+      Text due = new Text("Due ");
+
+      // "Friday, April 17 at 11:59 PM" - intended format
+      Text time =
+          new Text(assignment.getDueDate().format(DateTimeFormatter.ofPattern("EEEE, MMMM d"))
+              + " at " + assignment.getDueTime());
+      time.setId("time");
+
+      // Creates text for assignment description
+      Text desc = new Text(" - " + name);
+      assignmentBox.getChildren().addAll(due, time, desc);
+
+      // Adds assignment into to display
+      assignmentsPane.getChildren().add(assignmentBox);
+    }
+    
+    if (assignmentsByClass.get(className).toString().isEmpty()) {
       assignmentsPane.getChildren().add(new Text("There are no assignments in this class."));
-    } else {
-      Iterator<Assignment> today = assignmentsByClass.get(className).iterator();
-
-      // Creates an assignment box for each assignment in the class
-      while (today != null && today.hasNext()) {
-        Assignment assignment = today.next(); // Gets assignments with nearest due date
-        String name = assignment.getAssignmentName(); // Gets the assignment's name
-
-        // Creates an hbox and formats it
-        HBox assignmentBox = new HBox();
-        assignmentBox.getStyleClass().add("assignmentBox");
-
-        // Sets the color of the assignment to the class color
-        int[] classColor = assignment.getClassName().getClassColor();
-        assignmentBox.setStyle("-fx-background-color: rgba(" + classColor[0] + ", " + classColor[1]
-            + ", " + classColor[2] + ", 0.15); -fx-border-color: rgb(" + classColor[0] + ", "
-            + classColor[1] + ", " + classColor[2] + ");");
-
-        // When clicked, the assignment information appears
-        assignmentBox.setOnMouseClicked(e -> {
-          Main.assignmentOptions(assignmentBox, assignment);
-        });
-
-        // Creates text for assignment due date
-        Text due = new Text("Due ");
-
-        // "Friday, April 17 at 11:59 PM" - intended format
-        Text time =
-            new Text(assignment.getDueDate().format(DateTimeFormatter.ofPattern("EEEE, MMMM d"))
-                + " at " + assignment.getDueTime());
-        time.setId("time");
-
-        // Creates text for assignment description
-        Text desc = new Text(" - " + name);
-        assignmentBox.getChildren().addAll(due, time, desc);
-
-        // Adds assignment into to display
-        assignmentsPane.getChildren().add(assignmentBox);
-      }
     }
 
     // Creates scene and formats it
